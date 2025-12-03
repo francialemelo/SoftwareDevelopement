29d28d841dfd8f733b58762775eecccc407236ae
Franciale Melo <88666934+francialemelo@users.noreply.github.com>
2025-12-03 10:39:10 +0000

    labwed

--- lab11/exercise7.java
+++ lab11/exercise7.java
@@ -2,9 +2,9 @@
 /*Exercise 7:
 Write a Java program that simulates a simple ATM. The program should take an initial balance
 and allow the user to perform the following operations:
-• Check balance
-• Deposit money
-• Withdraw money*/
+• Check balance C
+• Deposit money D
+• Withdraw money W*/
 
 package lab11;
 import java.util.Scanner;
@@ -12,7 +12,46 @@ import java.util.Scanner;
 public class exercise7 {
     public static void main(String[] args) {
         Scanner input= new Scanner(System.in);
-        
+        double balance1, balance2;
+        String opration; 
+          System.out.print("Enter the inicial Balance ");
+          balance1=input.nextDouble();
+          System.out.print("Enter the operation • Check balance - C\r\n" + //
+                            "• Deposit money - D\r\n" + //
+                            "• Withdraw money - W");
+          opration = input.next();
+
+          if(opration.equals("C")){
+             System.out.print("Balance is "+ balance1 );
+ 
+          }else if (opration.equals("D")){
+            System.out.print("Enter your deposite value ");
+            balance2=input.nextDouble();
+
+            System.out.print("your blanece is " + (balance1+balance2));
+
+
+          } else if (opration.equals("W")) {
+             System.out.print("Enter your Withdraw value ");
+            balance2=input.nextDouble();
+            if (balance2 > balance1) {
+                System.out.print("Insuficient funds");
+
+            } else {
+            System.out.print("your blanece is " + (balance1-balance2)); }
+
+          } else {
+              System.out.print("Not a operation ");
+
+          }
+         
+
+    input.close();
+
+
+
+
+
     }
     
 }
---
+++ lab11/exercise8.java
@@ -0,0 +1,25 @@
+/*Write a program that calculates Body Mass Index (BMI) and uses cascading if/else to classify it:
+• BMI < 18.5: "Underweight"
+• BMI 18.5–24.9: "Normal weight"
+• BMI 25–29.9: "Overweight"
+• BMI >= 30: "Obesity"
+Note: BMI calculation is weight (kg) / height (𝑚𝑒𝑡𝑟𝑒2
+). Prompt user for height in metres and
+weight in kilograms.
+Print BMI classification result to the screen. */
+
+package lab11;
+import java.util.Scanner;
+public class exercise8 {
+    public static void main(String[] args) {
+        Scanner input= new Scanner(System.in);
+        double weight, height;
+        String opration; 
+          System.out.print("Enter the weight KG");
+          weight=input.nextDouble();
+          System.out.print("Enter the height metre2");
+          height=input.nextDouble();
+        
+    }
+    
+}
